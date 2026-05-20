664348a31aea04f471596095c5840e629ba72b30
Kacper3215641 <105670438+kapi54312358@users.noreply.github.com>
2026-05-20 21:02:08 +0200

    Create 3.3.cpp

---
+++ 3.3.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int const n=10000;
+
+bool czy_cyfra(char a){
+	if(a>=48 && a<=57)
+		return true;
+	else
+		return false;
+}
+
+int main(int argc, char** argv) {
+	ifstream we("C:/cpp/dane.txt");
+	string ciag="", l="";
+	vector <string> liczby, numery_tel;
+	int i=0, j=0, w=0;
+	
+	we>>ciag;
+	
+	for(i=0; i<n; i++){
+		if(czy_cyfra(ciag[i])){
+			l="";
+			l+=ciag[i];
+			j=i+1;
+			while(czy_cyfra(ciag[j])){
+				l+=ciag[j];
+				j++;
+			}
+			liczby.push_back(l);
+			i=j;
+		}
+	}
+
+	for(i=0; i<liczby.size(); i++){
+		l=liczby[i];
+		if(l.size()==9 && l[0]=='5')
+			cout<<l<<endl;
+	}
+	
+	return 0;
+}
